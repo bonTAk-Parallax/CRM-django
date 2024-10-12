2b7d7a740f136278b09108e9db7fd606d71d299a
bonTAK-Parallax <tanishup119@gmail.com>
2024-10-12 23:11:49 +0545

    2.5/4 done

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .forms import SignUpForm
+from .models import Record
 
 # Create your views here.
 
 def home(request):
+    records = Record.objects.all()
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
@@ -18,7 +20,7 @@ def home(request):
             messages.success(request,"There was an error logging in, please try again")
             return redirect('home')
     else:
-        return render(request, 'home.html',{})
+        return render(request, 'home.html',{'records':records})
 
 
 def logout_user(request):
@@ -34,7 +36,7 @@ def register_user(request):
 
             username = form.cleaned_data['username']
             password = form.cleaned_data['password1']
-            user = authenticate(username=username, password=password)
+            user = authenticate(request,username=username, password=password)
             login(request, user)
             messages.success(request, "You have successfully registered!!!")
             return redirect('home')
